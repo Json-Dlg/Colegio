2a1c3f8e493a2b3284cd42ee02efe32eec2783e1
.Json <61526077+Json-Dlg@users.noreply.github.com>
2020-10-13 18:37:07 -0500

    Creación Proyecto

---
+++ src/sfuturo/SFuturo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sfuturo;
+
+/**
+ *
+ * @author Jeiss
+ */
+public class SFuturo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        conexionBD conn = new conexionBD();
+    }
+    
+}
---
+++ src/sfuturo/conexionBD.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sfuturo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Jeiss
+ */
+public class conexionBD {
+     Connection con;
+     public conexionBD(){
+        try {
+             Class.forName("com.mysql.jdbc.Driver");
+             con  =  DriverManager.getConnection("jdbc:mysql://localhost:3306/ejemplo", "root", "");
+             System.out.println("Conexión exitosa");
+        } catch (Exception e) {
+            System.err.println("Error: "+ e);
+        }
+    }
+    
+}
